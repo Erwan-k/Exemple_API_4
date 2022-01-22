1ed593bfaf1e8e0033766c5860172a1edb2796b1
Erwan <kerbrat.erwan@gmail.com>
2022-01-22 10:00:26 +0100

    ca fonctionne, mais il n'y a pas de structuration de fichiers ni de data-init

--- api/main.py
+++ api/main.py
@@ -16,15 +16,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
 class Utilisateur(db.Model):
-  user = db.Column(db.String(255), unique=True, nullable=False, primary_key=True)
-  mdp = db.Column(db.String(255), nullable=False)
+	user = db.Column(db.String(255), unique=True, nullable=False, primary_key=True)
+	mdp = db.Column(db.String(255), nullable=False)
 
-  def __init__(self, user, mdp):
-    self.user = user
-    self.mdp = mdp
+	def __init__(self, user, mdp):
+		self.user = user
+		self.mdp = mdp
 
 db.create_all()
-
 
 exemple_route_get_args = reqparse.RequestParser()
 exemple_route_get_args.add_argument("user",type=str,required=True)
@@ -38,42 +37,41 @@ exemple_route_delete_args = reqparse.RequestParser()
 exemple_route_delete_args.add_argument("user",type=str,required=True)
 
 class exemple_route(Resource):
-  def get(self):
-    body = exemple_route_get_args.parse_args()
-    [user] = [body[i] for i in body]
-    user = Utilisateur.query.get(user)
-    del user.__dict__['_sa_instance_state']
-    return jsonify({"retour":"ok","valeur":user.__dict__["mdp"]})
+	def get(self):
+		body = exemple_route_get_args.parse_args()
+		[user] = [body[i] for i in body]
+		user = Utilisateur.query.get(user)
+		del user.__dict__['_sa_instance_state']
+		return jsonify({"retour":"ok","valeur":user.__dict__["mdp"]})
 
-  def post(self):
-    body = exemple_route_post_args.parse_args()
-    [user,password] = [body[i] for i in body]
-    db.session.add(Utilisateur(user,password))
-    db.session.commit()
-    return jsonify({"retour":"ok"})
+	def post(self):
+		body = exemple_route_post_args.parse_args()
+		[user,password] = [body[i] for i in body]
+		db.session.add(Utilisateur(user,password))
+		db.session.commit()
+		return jsonify({"retour":"ok"})
 
-  def put(self):
-    body = exemple_route_put_args.parse_args()
-    [user,password] = [body[i] for i in body]
-    db.session.query(Utilisateur).filter_by(user=user).update(dict(mdp=body['password']))
-    db.session.commit()
-    return jsonify({"retour":"ok"})
+	def put(self):
+		body = exemple_route_put_args.parse_args()
+		[user,password] = [body[i] for i in body]
+		db.session.query(Utilisateur).filter_by(user=user).update(dict(mdp=body['password']))
+		db.session.commit()
+		return jsonify({"retour":"ok"})
 
-  def delete(self):
-    body = exemple_route_delete_args.parse_args()
-    [user] = [body[i] for i in body]
-    db.session.query(Utilisateur).filter_by(user=user).delete()
-    db.session.commit()
-    return jsonify({"retour":"ok"})
-
+	def delete(self):
+		body = exemple_route_delete_args.parse_args()
+		[user] = [body[i] for i in body]
+		db.session.query(Utilisateur).filter_by(user=user).delete()
+		db.session.commit()
+		return jsonify({"retour":"ok"})
 
 class exemple_route_all(Resource):
-  def get(self):
-    users = []
-    for user in db.session.query(Utilisateur).all():
-      del user.__dict__['_sa_instance_state']
-      users += [user.__dict__]
-    return jsonify({"retour":"ok","valeur":users})
+	def get(self):
+		users = []
+		for user in db.session.query(Utilisateur).all():
+			del user.__dict__['_sa_instance_state']
+			users += [user.__dict__]
+		return jsonify({"retour":"ok","valeur":users})
 
 api.add_resource(exemple_route_all,"/exemple_route_all")
 api.add_resource(exemple_route,"/exemple_route")
--- api/utils.py
+++
@@ -1,9 +0,0 @@
-def getMysqlConnection():
-	url = make_url(os.getenv('DATABASE_URL'))
-	mydb = mysql.connector.connect(host=url.host,user=url.username,passwd=url.password,database=url.database)
-	return mydb
-
-def ajouter_user(user,mdp,mycursor,mydb):
-	val = (user,mdp)
-	mycursor.execute("INSERT INTO Utilisateur (user,mdp) VALUES ("+",".join(["%s"]*len(val))+")", val)
-	mydb.commit()
